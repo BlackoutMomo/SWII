b7aa2465354a674547725cc213d3bdd40f43c241
Sergio Gabriel <132919426+BlackoutMomo@users.noreply.github.com>
2024-05-17 11:24:09 +0000

    criando o sistema

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 function Entrar(){
     var resp = document.getElementById("resposta")
-    resp.innerHTML = "Entrado com sucesso"
+    var email = document.getElementById("email").value
+    var senha = document.getElementById("senha").value
+
+    var usuarios = new Map()
+
+    usuarios.set("usuario", "Sergio")
+    usuarios.set("email", "gabriel")
+    usuarios.set("senha", "barbosa")
+
+    if (usuarios.has("Sergio")){
+        resp.innerHTML("tem esse cadastro")
+    }
+
 }
