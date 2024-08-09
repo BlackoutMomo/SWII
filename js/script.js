fc7ad07ef4e8281b433c0aa7fed4fa4ef3400526
BlackoutMomo <sergiogabriel2606g@outlook.com>
2024-08-09 16:15:33 -0300

    receba

--- js/script.js
+++ js/script.js
@@ -3,77 +3,88 @@ const pokemonName = document.querySelector('.pokemon__name');
 const pokemonNumber = document.querySelector(".pokemon__number");
 const pokemonImage = document.querySelector(".pokemon__image");
 const pokemonHeight = document.querySelector('.pokemon__height');
-const pokemonWeight = document.querySelector('.pokemon_weight')
+const pokemonWeight = document.querySelector('.pokemon_weight');
 
 const form = document.querySelector('.form');
-const input = document.querySelector(".input__search")
+const input = document.querySelector(".input__search");
 
-let searchPokemon = 1
+console.log("receba")
+let searchPokemon = 1;
 
 //função que busca os dados do pokemon na API
 const fetchPokemon = async (pokemon) => {
     //faz uma requisição à api pokemon
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     //faz uma requisição foi bem-sucedida
-    if(APIResponse.status === 200){
+    if (APIResponse.status === 200) {
         //converte a resposta para json
-        const data = await APIResponse.json
-        return data
+        const data = await APIResponse.json();
+        return data;
     }
 }
 
 //função assincrona que renderiza os dados do pokemon na pagina
-const renderPokemon = async(pokemon) => {
+const renderPokemon = async (pokemon) => {
     //exibe mensagem de pesquisando
     pokemonName.innerHTML = "Pesquisando..."
-    pokemonNumber.innerHTML = ""
+    pokemonNumber.innerHTML = " ";
 
     const data = await fetchPokemon(pokemon);
 
-    if(data){
-        pokemonImage.computedStyleMap.display = 'block'
-        
-        pokemonNumber.innerHTML = data.id + ' -'
+    if (data) {
+        pokemonImage.style.display = 'block';
+
+        pokemonNumber.innerHTML = data.id + ' -';
         pokemonName.innerHTML = data.name;
 
-        pokemonHeight.innerHTML = "Altura" + ' ' + (data.height * 0.1).toFixed(2) + ' ' + 'metros'
-        pokemonWeight.innerHTML = "Peso:" + (data.weight / 10) + "kg"
+        pokemonHeight.innerHTML = "Altura" + ' ' + (data.height * 0.1).toFixed(2) + ' ' + 'metro(s)';
+        pokemonWeight.innerHTML = "Peso:" + data.weight / 10 + "kg";
 
-        if(data.id >= 1 && data.id <= 649){
-            pokemonImage.src = data['sprites']['versions']['generations-v']['black-white']['animated']['front-default'];   
-                    }
-
-        if(data.id >= 650 && data.id <= 721){
-            pokemonImage.src = data['sprites']['versions']['generations-vi']['x-y']['front-default'];   
-            document.querySelector('pokemon__image').computedStyleMap.width = "16%"
-            document.querySelector('.pokemon__image').computedStyleMap.height = '14%'
-            document.querySelector(".pokemon__image").computedStyleMap.bottom = "50%"
+        if (data.id >= 1 && data.id <= 649) {
+            pokemonImage.src = 
+            data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
         }
-        
-        if(data.id >= 722 && data.id <= 905){
-            pokemonImage.src = data['sprites']['versions']['generations-viii']['x-y']['front-default'];   
-            document.querySelector('pokemon__image').computedStyleMap.width = "16%"
-            document.querySelector('.pokemon__image').computedStyleMap.height = '14%'
-            document.querySelector(".pokemon__image").computedStyleMap.bottom = "49.5%"
+
+        if (data.id >= 650 && data.id <= 721) {
+            pokemonImage.src = data['sprites']['versions']['generation-vi']['x-y']['front_default'];
+            document.querySelector('.pokemon__image').style.width = "10%";
+            document.querySelector('.pokemon__image').style.height = '15%';
+            document.querySelector('.pokemon__image').style.bottom = "50%";
+        }
+
+        if (data.id >= 721 && data.id <= 809) {
+            pokemonImage.src = data['sprites']['versions']['generation-vii']['icons']['front_default'];
+            document.querySelector('.pokemon__image').style.width = "14%";
+            document.querySelector('.pokemon__image').style.height = '16%';
+            document.querySelector(".pokemon__image").style.bottom = "50%";
+        }
+
+        if (data.id >= 809 && data.id <= 905) {
+            pokemonImage.src = data['sprites']['versions']['generation-viii']['icons']['front_default'];
+            document.querySelector('.pokemon__image').style.width = "14%";
+            document.querySelector('.pokemon__image').style.height = '16%';
+            document.querySelector(".pokemon__image").style.bottom = "49.5%";
         }
         //limpa o campo de entrada
-        input.value = ''
+        input.value = '';
         //atualiza o numero pesquisado
-        searchPokemon = data.id
-        return data.id
+        searchPokemon = data.id;
+        return data.id;
     }
-    else{
-        pokemonImage.computedStyleMap.display = "none"
-        pokemonName.innerHTML = "tente novamente"
+    else {
+        pokemonImage.style.display = "none";
+        pokemonName.innerHTML = "tente novamente";
         pokemonNumber.innerHTML = ''
-        pokemonHeight.innerHTML = '---------------------'
-        pokemonWeight.innerHTML = '---------------------'
+        pokemonHeight.innerHTML = '---------------------';
+        pokemonWeight.innerHTML = '---------------------';
     }
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();//previne o comportamento padrão de recarregar
-        renderPokemon(input.value.toLowerCase())
-    })
-
-    renderPokemon(searchPokemon);
+   
 }
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();//previne o comportamento padrão de recarregar
+    renderPokemon(input.value.toLowerCase());
+})
+
+renderPokemon(searchPokemon);
