bf170f55a875bcde83e3020e889ae2380540e993
BlackoutMomo <sergiogabriel2606g@outlook.com>
2024-08-14 12:25:19 -0300

    meu deus

---
+++ js/validation.js
@@ -0,0 +1,18 @@
+var email = document.getElementById("email").value
+var senha = document.getElementById("senha").value
+
+function mostrar(){
+    console.log(email)
+    console.log(senha)
+}
+
+
+// const form = {
+//     email: () => document.getElementById("email"),
+//     senha: () => document.getElementById("senha"),
+//     erro_senha: () => document.getElementById("erro-senha"),
+//     erro_email_invalido: () => document.getElementById("erro-login-invalido"),
+//     erro_email_vazio: () => document.getElementById("erro-login"),
+//     recuperar_senha: () => document.getElementById("recover-password"),
+//     entrar: () => document.getElementById("entrar"),
+// }
