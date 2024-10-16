4ae2466339713d61fc60d554380fe0a007b0f3fe
BlackoutMomo <sergiogabriel2511g@gmail.com>
2024-10-16 11:07:43 -0300

    login_google

--- appLogin.js
+++ appLogin.js
@@ -7,53 +7,74 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDeQj2nE0XxrYu4qvNACectts5Et2eF-Hg",
-    authDomain: "blackoutmomo-f01c9.firebaseapp.com",
-    databaseURL: "https://blackoutmomo-f01c9-default-rtdb.firebaseio.com",
-    projectId: "blackoutmomo-f01c9",
-    storageBucket: "blackoutmomo-f01c9.appspot.com",
-    messagingSenderId: "97189801897",
-    appId: "1:97189801897:web:2a4f1b3237cbdf3a7b1eb8",
-    measurementId: "G-JWNQYLC70S",
-  };
-  
-  // Initialize Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  
-  var resposta = document.getElementById("resposta").innerHTML;
-  
-  var respostaCor = document.getElementById("resposta").style.color;
-  // Manipulação do formulário de login
-  function Entrar() {
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
-    console.log(email);
-    console.log(senha);
-  
-    auth
-      .signInWithEmailAndPassword(email, senha)
-      .then((userCredential) => {
-        // Login bem-sucedido
-        const user = userCredential.user;
-        document.getElementById("resposta").style.color = "green";
-        document.getElementById("resposta").innerHTML =
-          "Cadastro feito";
-  
-        // Redirecionar ou mostrar conteúdo para usuário autenticado
-      })
-      .catch((error) => {
-        // Exibe mensagem de erro
-        const errorCode = error.code;
-        const errorMessage = error.message;
-  
-        document.getElementById("resposta").style.color = "red";
-        document.getElementById("resposta").innerHTML = errorMessage;
-        alert("Erro: " + errorMessage);
-      });
+  apiKey: "AIzaSyDeQj2nE0XxrYu4qvNACectts5Et2eF-Hg",
+  authDomain: "blackoutmomo-f01c9.firebaseapp.com",
+  databaseURL: "https://blackoutmomo-f01c9-default-rtdb.firebaseio.com",
+  projectId: "blackoutmomo-f01c9",
+  storageBucket: "blackoutmomo-f01c9.appspot.com",
+  messagingSenderId: "97189801897",
+  appId: "1:97189801897:web:2a4f1b3237cbdf3a7b1eb8",
+  measurementId: "G-JWNQYLC70S",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+var resposta = document.getElementById("resposta").innerHTML;
+
+var respostaCor = document.getElementById("resposta").style.color;
+// Manipulação do formulário de login
+function Entrar() {
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+  console.log(email);
+  console.log(senha);
+
+  auth
+    .signInWithEmailAndPassword(email, senha)
+    .then((userCredential) => {
+      // Login bem-sucedido
+      const user = userCredential.user;
+      document.getElementById("resposta").style.color = "green";
+      document.getElementById("resposta").innerHTML =
+        "Cadastro feito";
+
+      // Redirecionar ou mostrar conteúdo para usuário autenticado
+    })
+    .catch((error) => {
+      // Exibe mensagem de erro
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+      document.getElementById("resposta").style.color = "red";
+      document.getElementById("resposta").innerHTML = errorMessage;
+      alert("Erro: " + errorMessage);
+    });
 
 
-  }
-  
-  
-  
+}
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+function googleLogin() {
+  firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      var credential = result.credential;
+      var token = credential.accessToken;
+      var user = result.user;
+      document.getElementById("resposta").style.color = "green";
+      document.getElementById("resposta").innerHTML =
+        "Cadastro feito";
+    }).catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+      document.getElementById("resposta").innerHTML = errorMessage;
+      alert("Erro: " + errorMessage);
+    });
+
+}
+
